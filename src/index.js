b2db1a49c9b167a995732a54f553f79efff5ed1c
cyn <1909178414@qq.com>
2018-05-10 10:02:25 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+//HMR 模块热替换,浏览器不用重新刷新
+if (module.hot) {
+    module.hot.accept();
+  }
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
