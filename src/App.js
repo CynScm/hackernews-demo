978b0ab7cc6b9ba9781f451cf58f95756548c756
cyn <1909178414@qq.com>
2018-05-13 23:45:33 +0800

    react 样式

--- src/App.js
+++ src/App.js
@@ -51,30 +51,65 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       //元素（element） JSX
-    <div className="App"  > 
-    <form>
-          <input value={searchTerm} onChange={this.onSearchChange} type="text" />
-    </form>
-      {list.filter(isSearched(searchTerm)).map((item)=>{
-        return (
-        <div key={item.objectID}> <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-              <button
-                // onClick={(this.onDismiss}
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"> Dismiss
-              </button>
-            </span>
-</div>)
-      })}
+    <div className="page"  > 
+      <div className="interactions">
+      
+    <Search   value={searchTerm} onChange = {this.onSearchChange}>Search</Search>
+    <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+ 
     </div>
     );
   }
+}
+class Button extends Component { render() {
+  const { onClick, className, children,
+  } = this.props;
+  return ( 
+      <button
+          onClick={onClick}
+          className={className}
+          type="button">
+          {children}
+       </button> 
+  );
+  } }
+class Search extends Component {
+  render() {
+    const { value, onChange,children } = this.props;
+    return (
+      <form>{children}
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}/> 
+      </form>
+    ); }
+}
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div className="table">
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID} className="table-row">
+            <span style={{ width: '40%' }}> 
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={{ width: '30%' }}>{item.author}</span>
+            <span style={{ width: '10%' }}>{item.num_comments}</span>
+            <span style={{ width: '10%' }}>{item.points}</span>
+            <span style={{ width: '10%' }}>
+            <Button className="button-inline" onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+            </Button>
+            </span>
+            </div> 
+          )
+        }
+    </div> )
+    ;
+  } 
 }
 
 export default App;
